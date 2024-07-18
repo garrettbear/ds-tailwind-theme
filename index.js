0a080d6c4641d4cb1192265076f363f97a3e25f5
garrettbear <gwhisten@gmail.com>
2024-07-18 16:03:25 -0700

    tailwind-theme-plugin inti

---
+++ index.js
@@ -0,0 +1,28 @@
+const plugin = require("tailwindcss/plugin");
+
+module.exports = plugin(
+  function ({ addBase, theme }) {
+    addBase({
+      h1: { fontSize: theme("fontSize.2xl") },
+      h2: { fontSize: theme("fontSize.xl") },
+      // Add more base styles or components as needed
+    });
+  },
+  {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1D4ED8",
+          secondary: "#9333EA",
+          // Add your custom design tokens here
+        },
+        spacing: {
+          72: "18rem",
+          84: "21rem",
+          96: "24rem",
+          // Add more custom spacing values
+        },
+      },
+    },
+  }
+);
