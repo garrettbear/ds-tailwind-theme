ebffd23bef0a1682e222a5e16711f573dc4e8461
garrettbear <gwhisten@gmail.com>
2024-07-26 14:56:10 -0700

    plugin test

--- demo-site/tailwind.config.ts
+++ demo-site/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const dsTailwindTheme = require("ds-tailwind-theme");
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("../index.js")],
+  plugins: [dsTailwindTheme],
 };
 export default config;
