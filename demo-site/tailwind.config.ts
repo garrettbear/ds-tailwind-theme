7ababb27f9aa275d937d6c0e1d92859e521b339b
garrettbear <gwhisten@gmail.com>
2024-07-29 08:58:53 -0700

    env for local changes

--- demo-site/tailwind.config.ts
+++ demo-site/tailwind.config.ts
@@ -1,21 +1,24 @@
-import type { Config } from "tailwindcss";
-const dsTailwindTheme = require("ds-tailwind-theme");
+import type { Config } from 'tailwindcss';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const dsTailwindTheme = process.env.LOCAL_ENV === 'true' ? require('../index.js') : require('ds-tailwind-theme');
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [dsTailwindTheme],
 };
+
 export default config;
